49c46601b05ee77deb0d73ee33c69c7f7c675512
febaemanuel <febaemanuel17@gmail.com>
2025-12-26 23:36:31 -0300

    Delete Downloads directory

--- Downloads/upload2/migrate_add_task_id.sql
+++
@@ -1,9 +0,0 @@
--- Adiciona coluna task_id na tabela campanhas para rastrear tasks Celery
--- Execute este script se estiver usando banco existente
-
-ALTER TABLE campanhas ADD COLUMN IF NOT EXISTS task_id VARCHAR(100);
-
--- Verificar se foi adicionada
-SELECT column_name, data_type
-FROM information_schema.columns
-WHERE table_name = 'campanhas' AND column_name = 'task_id';
